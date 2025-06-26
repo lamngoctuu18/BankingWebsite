5ba63e7c39654a996f21bbdfc17f9965c0f75d21
trantriduy0511 <trandui2004@gmail.com>
2025-06-26 09:50:32 +0700

    Thêm trang About pages và reponsive

--- src/pages/Security.jsx
+++ src/pages/Security.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-
 const Security = () => {
   return (
     <div>
-      <h1>Security Page</h1>
-      <p>Learn about our security measures.</p>
+        <h1>Security Page</h1>
+        <p></p>
     </div>
   );
 };
